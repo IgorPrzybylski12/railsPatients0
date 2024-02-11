93bc8d6b4bca29b9f66b4e8972702e72c3dcd9cd
Igor <igorprzybylski127@gmail.com>
2024-02-11 23:52:18 +0100

    no_other_visits_between_beginning_and_ending

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -1,3 +1,16 @@
 class Visit < ApplicationRecord
   belongs_to :user
+
+  validate :no_other_visits_between_beginning_and_ending
+
+  private
+
+  def no_other_visits_between_beginning_and_ending
+    if Visit.where(user_id: user_id)
+            .where.not(id: id) # exclude current visit if updating
+            .where("begining <= ? AND ending >= ?", ending, begining)
+            .exists?
+      errors.add(:base, "There is another visit for this user between the beginning and ending times")
+    end
+  end
 end
