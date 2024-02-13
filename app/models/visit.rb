5f5dadc7c703ef62dad9a4989cde31f585540abe
IgorPrzybylski12 <118221077+IgorPrzybylski12@users.noreply.github.com>
2024-02-13 15:44:12 +0100

    validate ending must be later the beginning

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -2,15 +2,22 @@ class Visit < ApplicationRecord
   belongs_to :doctor
 
   validate :no_other_visits_between_beginning_and_ending
+  validate :ending_must_be_later_the_beginning
 
   private
 
   def no_other_visits_between_beginning_and_ending
     if Visit.where(doctor_id: doctor_id)
-            .where.not(id: id) # exclude current visit if updating
+            .where.not(id: id) 
             .where("begining <= ? AND ending >= ?", ending, begining)
             .exists?
       errors.add(:base, "There is another visit for this user between the beginning and ending times")
+    end
+  end
+
+  def ending_must_be_later_the_beginning
+    if ending <= begining
+      errors.add(:base, "Ending must be later the beginning")
     end
   end
 end
